c97c03967e16c4e05207843f7079637e02247b85
gonzalomadnessdev <gonzalo.hr.albornoz@gmail.com>
2023-08-10 21:14:27 -0300

    test profesionales

---
+++ app/Api/V1/Controllers/TestController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Api\V1\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\Profesional;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+
+class TestController extends Controller
+{
+    public function mostrarProfesional($id)
+    {
+        $profesional = null;
+
+        try {
+            $profesional = Profesional::select()->find($id);
+            if (empty($profesional)) {
+                throw new HttpException(400, "El profesional requerido no existe.");
+            }
+        } catch (\Exception $e) {
+            throw $e;
+        }
+
+        return response()->json(
+            [
+                'status' => 'ok',
+                'profesional' => $profesional
+            ]
+        );
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('profesionales/{id}', [\App\Api\V1\Controllers\TestController::class, 'mostrarProfesional'])->where('id', '[0-9]+');
